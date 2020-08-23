5df612bf642c58236c62c7a3794b45e877457fe0
b3nj4m1n <b3nj4m1n@gmx.net>
2020-08-23 14:05:02 +0200

    Update ssl stuff

--- scripts/ssl-certs/cagen.sh
+++ scripts/ssl-certs/cagen.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-# Generates a private key
+# Generates ssl certificates suitable for use with android
+echo "basicConstraints=CA:true" > "android_options.txt"
 
-openssl genrsa -des3 -out "$1" 2048
+
+openssl genrsa -out priv_and_pub.key 2048
+openssl req -new -days 3650 -key priv_and_pub.key -out CA.pem
+openssl x509 -req -days 3650 -in CA.pem -signkey priv_and_pub.key -extfile ./android_options.txt -out CA.crt
+
+echo "Import CA.der.crt in android"
+openssl x509 -inform PEM -outform DER -in CA.crt -out CA.der.crt
